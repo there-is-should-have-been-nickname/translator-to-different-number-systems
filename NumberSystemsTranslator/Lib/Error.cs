85e969d29cb1d6fd0d38deb27b48b6d97dda392a
Diana <kozachenkodiana03@mail.ru>
2022-02-21 11:45:10 +0700

    Added Error class, reduced some code

---
+++ NumberSystemsTranslator/Lib/Error.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lib
+{
+    public static class Error
+    {
+        public static string GetErrorMessage(string notionFrom, string notionTo, string number)
+        {
+            if (string.IsNullOrWhiteSpace(number))
+            {
+                return "Вы не ввели число";
+            }
+
+            if (string.IsNullOrWhiteSpace(notionFrom))
+            {
+                return "Вы не ввели систему счисления, из которой будет осуществлен перевод";
+            }
+
+            if (string.IsNullOrWhiteSpace(notionTo))
+            {
+                return "Вы не ввели систему счисления, в которую будет осуществлен перевод";
+            }
+
+            if (number[0] == '0')
+            {
+                return "Число не должно начинаться с нуля";
+            }
+
+            if (number[0] == '.' || number[^1] == '.')
+            {
+                return "Точка должна быть между цифрами. Она не может быть ни первой, ни последней";
+            }
+
+            if (new Regex(@"[a-z!@#№$%^&*()\-+=?<>/|\\ ]+").Matches(number).Count > 0)
+            {
+                return "Число не должно содержать что-то кроме букв или цифр";
+            }
+
+            if (new Regex(@"[А-Яа-я]+").Matches(number).Count > 0)
+            {
+                return "Не должно быть русских букв";
+            }
+
+            return "";
+        }
+
+        public static Exception ThrowError(string message)
+        {
+            throw new Exception(message: message);
+        }
+    }
+}
--- NumberSystemsTranslator/NumberSystemsTranslator/MainWindow.xaml.cs
+++ NumberSystemsTranslator/NumberSystemsTranslator/MainWindow.xaml.cs
@@ -11,19 +11,6 @@ namespace NumberSystemsTranslator
     /// </summary>
     public partial class MainWindow : Window
     {
-        /// <summary>
-        /// Required variables
-        /// </summary>
-        private string Number = "";
-        private int NotionFrom = 0;
-        private int NotionTo = 0;
-        private string Result = "";
-
-        //private NumberFormatInfo Provider = new()
-        //{
-        //    NumberDecimalSeparator = "."
-        //};
-
         public MainWindow()
         {
             InitializeComponent();
@@ -33,73 +20,21 @@ namespace NumberSystemsTranslator
         {
             try
             {
-                Number = TextBoxNumber.Text;
-
-                string NotionFromStr = ComboBoxFrom.Text;
-                string NotionToStr = ComboBoxTo.Text;
-
                 //Checking and handling errors
-                if (!string.IsNullOrWhiteSpace(GetErrorMessage()))
+                if (!string.IsNullOrWhiteSpace(Error.GetErrorMessage(ComboBoxFrom.Text, ComboBoxTo.Text, TextBoxNumber.Text)))
                 {
-                    ThrowError(GetErrorMessage());
+                    Error.ThrowError(Error.GetErrorMessage(ComboBoxFrom.Text, ComboBoxTo.Text, TextBoxNumber.Text));
                 }
 
-                NotionFrom = Convert.ToInt32(NotionFromStr);
-                NotionTo = Convert.ToInt32(NotionToStr);
-                Result = Translator.Translate(NotionFrom, NotionTo, Number);
-
-                TextBoxResult.Text = Result;
+                TextBoxResult.Text = Translator.Translate(Convert.ToInt32(ComboBoxFrom.Text), 
+                    Convert.ToInt32(ComboBoxTo.Text), 
+                    TextBoxNumber.Text);
 
             }
             catch (Exception err)
             {
                 MessageBox.Show(err.Message);
             }
-        }
-
-        private string GetErrorMessage()
-        {
-            if (string.IsNullOrWhiteSpace(TextBoxNumber.Text))
-            {
-                return "Вы не ввели число";
-            }
-
-            if (string.IsNullOrWhiteSpace(ComboBoxFrom.Text))
-            {
-                return "Вы не ввели систему счисления, из которой будет осуществлен перевод";
-            }
-
-            if (string.IsNullOrWhiteSpace(ComboBoxTo.Text))
-            {
-                return "Вы не ввели систему счисления, в которую будет осуществлен перевод";
-            }
-
-            if (TextBoxNumber.Text[0] == '0')
-            {
-                return "Число не должно начинаться с нуля";
-            }
-
-            if (TextBoxNumber.Text[0] == '.' || TextBoxNumber.Text[^1] == '.')
-            {
-                return "Точка должна быть между цифрами. Она не может быть ни первой, ни последней";
-            }
-
-            if (new Regex(@"[a-z!@#№$%^&*()\-+=?<>/|\\ ]+").Matches(TextBoxNumber.Text).Count > 0)
-            {
-                return "Число не должно содержать что-то кроме букв или цифр";
-            }
-
-            if (new Regex(@"[А-Яа-я]+").Matches(TextBoxNumber.Text).Count > 0)
-            {
-                return "Не должно быть русских букв";
-            }
-
-            return "";
-        }
-
-        private static Exception ThrowError(string message)
-        {
-            throw new Exception(message: message);
         }
     }
 }
