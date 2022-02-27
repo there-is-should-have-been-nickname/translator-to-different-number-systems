ae168e8e8334f5cb65e9dc177d07d0ae0faf6ebc
Diana <kozachenkodiana03@mail.ru>
2022-02-27 18:41:41 +0700

    Add binding

---
+++ NumberSystemsTranslator/Lib/Variables.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib
+{
+    public class Variables
+    {
+        public string NotionFrom { get; set; }
+        public string NotionTo { get; set; }
+        public string NumberEntire { get; set; }
+
+        public Variables()
+        {
+
+        }
+    }
+}
--- NumberSystemsTranslator/NumberSystemsTranslator/MainWindow.xaml.cs
+++ NumberSystemsTranslator/NumberSystemsTranslator/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System.Globalization;
 using System.Text.RegularExpressions;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 using Lib;
 
 namespace NumberSystemsTranslator
@@ -11,9 +13,28 @@ namespace NumberSystemsTranslator
     /// </summary>
     public partial class MainWindow : Window
     {
+        Variables variables = new Variables();
         public MainWindow()
         {
             InitializeComponent();
+
+            variables = new Variables();
+            var bindingFrom = new Binding("NotionFrom")
+            {
+                Source = variables
+            };
+            var bindingTo = new Binding("NotionTo")
+            {
+                Source = variables
+            };
+            var bindingNumber = new Binding("NumberEntire")
+            {
+                Source = variables
+            };
+
+            ComboBoxFrom.SetBinding(ComboBox.TextProperty, bindingFrom);
+            ComboBoxTo.SetBinding(ComboBox.TextProperty, bindingTo);
+            TextBoxNumber.SetBinding(TextBox.TextProperty, bindingNumber);
         }
 
         private void ButtonCalculate_Click(object sender, RoutedEventArgs e)
